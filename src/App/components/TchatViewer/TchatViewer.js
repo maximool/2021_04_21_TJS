7748d84e0174cba17da25f3540f6b7df316e975c
alex <desorbaix@free.fr>
2021-04-22 14:16:48 +0200

    pulling

--- src/App/components/TchatViewer/TchatViewer.js
+++ src/App/components/TchatViewer/TchatViewer.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './TchatViewer.module.scss';
-
-const TchatViewer = (props) => (
-  <div className={styles.TchatViewer} data-testid="TchatViewer">
-    TchatViewer Component
-  </div>
-);
+import { REST_ADR } from '../../config/config';
+const TchatViewer = (props) => {
+  const [messages, setmessages] = useState([]);
+  const [lastId, setlastId] = useState(-1);
+  const [fetchCount, setfetchCount] = useState(0);
+  //component did mount avec [] reference de changement vide
+  useEffect(() => {
+    fetch(`${REST_ADR}/messages?id_gte=${lastId + 1}`)
+      .then(flux => flux.json(), flux => { console.log(flux); return [] })
+      .then(jsonArr => {
+        setTimeout(() => setfetchCount(fetchCount + 1), 3000);
+        if (jsonArr.length <= 0) {
+          return jsonArr;
+        }
+        let last = lastId;
+        jsonArr.forEach(e => { if (last < e.id) last = e.id })
+        if (lastId < last) {
+          setlastId(last);
+        }
+        console.log('message initialement recus', jsonArr);
+        setmessages([...messages, ...jsonArr]);
+        return jsonArr;
+      })
+  }, [fetchCount]);
+  return (
+    <div className={styles.TchatViewer} data-testid="TchatViewer">
+      {messages.map((e, i) => <div className="message" key={'message-' + i}>
+        {e.dateTime && e.dateTime.split('T')[1].split('.')[0] + ':'}<span style={{ color: e.color }}>{e.text}</span>
+        {/*2021-04-23T12:35:00.595*/}
+        {JSON.stringify(e)}
+      </div>)}
+    </div>
+  );
+}
 
 TchatViewer.propTypes = {};
 
