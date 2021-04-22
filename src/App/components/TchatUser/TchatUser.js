9407ceb343ccebb00da1dfc037c2d2184b2fc506
alex <desorbaix@free.fr>
2021-04-22 17:48:52 +0200

    mise en place du panneau de gauche

--- src/App/components/TchatUser/TchatUser.js
+++ src/App/components/TchatUser/TchatUser.js
@@ -4,11 +4,12 @@ import styles from './TchatUser.module.scss';
 
 const TchatUser = (props) => (
   <div className={styles.TchatUser} data-testid="TchatUser">
-    TchatUser Component
+    <img style={{height:'100px',maxWidth:'80px',size:'auto', display:'inline'}} src={props.user.img} alt={'face de '+props.user.login}/>
+    {props.user.login}
   </div>
 );
 
-TchatUser.propTypes = {};
+TchatUser.propTypes = {user:PropTypes.object.isRequired};
 
 TchatUser.defaultProps = {};
 
