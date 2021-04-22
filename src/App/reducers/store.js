eeea16b36d3316181e651127af741e0fe24d5c73
alex <desorbaix@free.fr>
2021-04-22 17:08:22 +0200

    add const and case typE init

--- src/App/reducers/store.js
+++ src/App/reducers/store.js
@@ -4,18 +4,49 @@ const initialState = {
     messages: [],
     tchatUsers: []
 }
+export const TCHAT_ACTIONS=Object.freeze({
+    ADD_USERS:'ADD_USERS',
+    ADD_USER:'ADD_USER',
+    ADD_MESSAGES:'ADD_MESSAGES',
+    SEND_MESSAGE:'SEND_MESSAGE'
+});
+const TCHAT_PRIVATE_ACTIONS=Object.freeze({
+    INIT:'@@redux/INIT',
+    INIT_PULLING:'INIT_PULLING',
+    PULLING:'PULLING'
+});
 function tchatReducer(state = initialState, action) {
-    console.log(action.type);
-    if(action.type.includes('@@redux/INIT')){action.type='@@redux/INIT';}
+   // console.log(action.type);
+    if(action.type.includes('@@redux/INIT')){action.type=TCHAT_PRIVATE_ACTIONS.INIT;}
     switch (action.type) {
-        case '@@redux/INIT':
+        case TCHAT_PRIVATE_ACTIONS.INIT:
             fetch(`${REST_ADR}/messages`).then(f=>f.json()).then(o=>{
-                store.dispatch({type:'ADD_MESSAGES',values:o});
+                store.dispatch({type:TCHAT_ACTIONS.ADD_MESSAGES,values:o});
+            });
+            fetch(`${REST_ADR}/tchatUsers`).then(f=>f.json()).then(o=>{
+                store.dispatch({type:TCHAT_ACTIONS.ADD_USERS,values:o});
             })
+            setInterval(()=>{store.dispatch({type:TCHAT_PRIVATE_ACTIONS.PULLING})},2000);
         return state;
-        case 'ADD_USER':return {...state,tchatUsers:[...state.tchatUsers,action.value]};
-        case 'ADD_USERS':return {...state,tchatUsers:[...state.tchatUsers,...action.values]};
-        case 'ADD_MESSAGES':return {...state,messages:[...state.messages,...action.values]};
+        case TCHAT_ACTIONS.SEND_MESSAGE:
+            fetch(`${REST_ADR}/messages`,{method:'POST',body:JSON.stringify(action.value)})
+                .then(f=>{console.log(f)},f=>{console.log(f)})
+            return state;
+        case TCHAT_PRIVATE_ACTIONS.PULLING:
+                let last=0;
+                console.log('pulling')
+                state.messages.forEach((e)=>{last=e.id>last?e.id:last;})
+                fetch(`${REST_ADR}/messages?id_gte=${last+1}`)
+                    .then(f=>f.json())
+                    .then(o=>{
+                        if (o.length <= 0) return;
+                        store.dispatch({type:TCHAT_ACTIONS.ADD_MESSAGES,values:o})
+                    });
+            return state;
+        case TCHAT_ACTIONS.ADD_USER:return {...state,tchatUsers:[...state.tchatUsers,action.value]};
+        case TCHAT_ACTIONS.ADD_USERS:return {...state,tchatUsers:[...state.tchatUsers,...action.values]};
+        case TCHAT_ACTIONS.ADD_MESSAGES:
+            return {...state,messages:[...state.messages,...action.values]};
         default: return state;
     }
 }
